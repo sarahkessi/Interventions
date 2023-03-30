30b56b29482f93b0ff614f7281229756ee63fa65
Sarah Kessi <2156390@cmontmorency.qc.ca>
2023-03-30 15:48:49 -0400

    #3 Zone prénom valide avec 200 caractères

--- src/app/probleme/probleme.component.spec.ts
+++ src/app/probleme/probleme.component.spec.ts
@@ -35,4 +35,10 @@ describe('ProblemeComponent', () => {
     zone.setValue('a'.repeat(3));
     expect(zone.valid).toBeTruthy();
   });
+
+  it('Zone prénom valide avec 200 caractères', () => {
+    let zone = component.problemeForm.controls['prenom'];
+    zone.setValue('a'.repeat(200));
+    expect(zone.valid).toBeTruthy();
+  });
 });
